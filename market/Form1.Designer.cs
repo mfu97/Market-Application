42ece08c5417919ceb2d1709b617ca033ad03382
mfu97 <mfu97@hotmail.com>
2021-11-14 19:28:10 +0300

    Initial commit

---
+++ market/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿using System;
+
+namespace market
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.urun_adi_satis = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.barkod_satıs = new System.Windows.Forms.TextBox();
+            this.satıs_fiyat = new System.Windows.Forms.TextBox();
+            this.satıs_btn = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.satıs_adet = new System.Windows.Forms.TextBox();
+            this.kalan_adet = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(67, 50);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1163, 188);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // urun_adi_satis
+            // 
+            this.urun_adi_satis.FormattingEnabled = true;
+            this.urun_adi_satis.Items.AddRange(new object[] {
+            "PEPSİ",
+            "ÜLKER",
+            "ETİ",
+            "LAYS",
+            "GONG"});
+            this.urun_adi_satis.Location = new System.Drawing.Point(179, 282);
+            this.urun_adi_satis.Name = "urun_adi_satis";
+            this.urun_adi_satis.Size = new System.Drawing.Size(121, 24);
+            this.urun_adi_satis.TabIndex = 10;
+            this.urun_adi_satis.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(64, 285);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 17);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "BRAND";
+            
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(64, 332);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 17);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "BARCODE";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(64, 370);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 17);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "PRICE";
+            // 
+            // barkod_satıs
+            // 
+            this.barkod_satıs.Location = new System.Drawing.Point(179, 327);
+            this.barkod_satıs.Name = "barkod_satıs";
+            this.barkod_satıs.Size = new System.Drawing.Size(100, 22);
+            this.barkod_satıs.TabIndex = 14;
+            // 
+            // satıs_fiyat
+            // 
+            this.satıs_fiyat.Location = new System.Drawing.Point(179, 365);
+            this.satıs_fiyat.Name = "satıs_fiyat";
+            this.satıs_fiyat.Size = new System.Drawing.Size(100, 22);
+            this.satıs_fiyat.TabIndex = 15;
+            // 
+            // satıs_btn
+            // 
+            this.satıs_btn.Location = new System.Drawing.Point(179, 444);
+            this.satıs_btn.Name = "satıs_btn";
+            this.satıs_btn.Size = new System.Drawing.Size(75, 23);
+            this.satıs_btn.TabIndex = 16;
+            this.satıs_btn.Text = "SALES";
+            this.satıs_btn.UseVisualStyleBackColor = true;
+            this.satıs_btn.Click += new System.EventHandler(this.satıs_btn_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(345, 282);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(885, 185);
+            this.dataGridView2.TabIndex = 17;
+            this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(64, 407);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 17);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "PIECE";
+            // 
+            // satıs_adet
+            // 
+            this.satıs_adet.Location = new System.Drawing.Point(179, 402);
+            this.satıs_adet.Name = "satıs_adet";
+            this.satıs_adet.Size = new System.Drawing.Size(100, 22);
+            this.satıs_adet.TabIndex = 19;
+            // 
+            // kalan_adet
+            // 
+            this.kalan_adet.Location = new System.Drawing.Point(1236, 50);
+            this.kalan_adet.Name = "kalan_adet";
+            this.kalan_adet.Size = new System.Drawing.Size(100, 22);
+            this.kalan_adet.TabIndex = 20;
+            this.kalan_adet.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1504, 623);
+            this.Controls.Add(this.kalan_adet);
+            this.Controls.Add(this.satıs_adet);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.satıs_btn);
+            this.Controls.Add(this.satıs_fiyat);
+            this.Controls.Add(this.barkod_satıs);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.urun_adi_satis);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox urun_adi_satis;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox barkod_satıs;
+        private System.Windows.Forms.TextBox satıs_fiyat;
+        private System.Windows.Forms.Button satıs_btn;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox satıs_adet;
+        private System.Windows.Forms.TextBox kalan_adet;
+    }
+}
+
---
+++ market/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace market
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DataTable tablo = new DataTable();
+        DataTable satis_tablo = new DataTable();
+        public int aranan_r = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            tablo.Columns.Add("BARCODE", typeof(int));
+            tablo.Columns.Add("BRAND", typeof(string));
+            tablo.Columns.Add("PRICE ($)", typeof(int));
+            tablo.Columns.Add("PRODUCT NAME", typeof(string));
+            tablo.Columns.Add("PIECE", typeof(int));
+
+            satis_tablo.Columns.Add("BARCODE", typeof(string));
+            satis_tablo.Columns.Add("BRAND", typeof(string));
+            satis_tablo.Columns.Add("PRICE ($)", typeof(string));
+            satis_tablo.Columns.Add("PIECE", typeof(string));
+            satis_tablo.Columns.Add("TOTAL ($)", typeof(string));
+
+            dataGridView1.DataSource = tablo;
+            tablo.Rows.Add("147258", "PEPSİ","8", "COKE 1LT", "19");
+            tablo.Rows.Add("236584", "ÜLKER", "2", "CHOCLATE BAR", "18");
+            tablo.Rows.Add("135789", "ETİ", "3", "BISCUIT", "11");
+            tablo.Rows.Add("142653", "LAYS", "9", "CRISPS 125GR", "16");
+            tablo.Rows.Add("128467", "ALPELLA", "3", "CHOCLATE MILK", "17");
+        }        
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string aranan = urun_adi_satis.Text.Trim().ToUpper();
+
+            
+
+            for (int i = 0; i <= dataGridView1.Rows.Count - 1; i++)
+            {
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    foreach (DataGridViewCell cell in dataGridView1.Rows[i].Cells)
+                    {
+                        if (cell.Value != null)
+                        {
+                            if (cell.Value.ToString().ToUpper() == aranan)
+                            {
+                                aranan_r = i;                                
+                            }
+                        }
+                    }                    
+                }                
+            }
+            barkod_satıs.Text = dataGridView1.Rows[aranan_r].Cells[0].Value.ToString();
+            satıs_fiyat.Text = dataGridView1.Rows[aranan_r].Cells[2].Value.ToString();
+            kalan_adet.Text = dataGridView1.Rows[aranan_r].Cells[4].Value.ToString();
+
+        }
+
+        private void satıs_btn_Click(object sender, EventArgs e)
+        {
+
+            dataGridView2.DataSource = satis_tablo;
+            double para = Convert.ToDouble(satıs_fiyat.Text) * Convert.ToDouble(satıs_adet.Text);
+            satis_tablo.Rows.Add(barkod_satıs.Text, urun_adi_satis.Text, satıs_fiyat.Text, satıs_adet.Text, para);
+
+            int stok = Convert.ToInt16(kalan_adet.Text) - Convert.ToInt16(satıs_adet.Text);
+            kalan_adet.Text = Convert.ToString(stok);
+
+
+            dataGridView1.Rows[aranan_r].Cells[4].Value = stok;
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            string urun_adi = urun_adi_satis.SelectedItem.ToString();
+            
+
+        }
+
+        
+    }
+}
